4eed058c427ab3d445afa0707bc26efd1582408d
James Antrobus <jamesantrobus@gmail.com>
2012-07-26 18:23:46 +0100

    tiny mvc4 project for testing

---
+++ LoadTestDemoMVC4/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿namespace LoadTestDemoMVC4.Controllers
+{
+	using System.Web.Mvc;
+
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+			var viewModel = new HomeViewModel();
+
+			return View(viewModel);
+		}
+
+		[ValidateAntiForgeryToken]
+		[HttpPost]
+		public ActionResult Index(HomeViewModel viewModel)
+		{
+			return RedirectToAction("AnotherPage");
+		}
+
+		[HttpGet]
+		public ActionResult AnotherPage()
+		{
+			return View();
+		}
+	}
+
+	public class HomeViewModel
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ LoadTestDemoMVC4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoadTestDemoMVC4.MvcApplication" Language="C#" %>
